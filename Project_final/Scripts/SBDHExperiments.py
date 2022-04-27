3cb851e5089de7eece150c81931f0271a7107a9c
Vijeta <vkdeshpande@umass.edu>
2022-04-27 04:48:18 -0400

    taking script out of the debug mode

--- Project_final/Scripts/SBDHExperiments.py
+++ Project_final/Scripts/SBDHExperiments.py
@@ -52,7 +52,7 @@ def parse_args():
     )
     parser.add_argument(
         "--debug",
-        default=True,
+        default=False,
         action="store_true",
         help="Whether to use a small subset of the dataset for debugging.",
     )
